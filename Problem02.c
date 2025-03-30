3c7c065bd732c4892cdf6dc2a9e6d4e8a558a067
Kishor Roy <111331405+numbkishor@users.noreply.github.com>
2025-03-30 22:51:04 +0600

    Problem02.c

---
+++ Problem02.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    double pi=3.1416;
+    int m1=3, m2=2;
+     double d,lembda_1=530, lembda_2=700, th_1=65, th_2;
+     th_1=th_1*(pi/180);
+     d=(lembda_1 * m1)/(sin(th_1));
+     th_2=asin((lembda_2 * m2)/d)*(180/pi);
+     printf("The angle for second bright spot will be: %lf", th_2);
+    return 0;
+}
